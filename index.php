682747772827339a698b33da552300b026f793d5
malagonius <47065208+malagonius@users.noreply.github.com>
2021-12-04 19:15:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<body>
-    Per procedere con l'iscrizione premere uno dei due seguenti link: <br/>
-    <a href="https://form.jotform.com/213096726972364">Link 1 </a>  <br/>
-    <a href="https://form.jotform.com/213375514018350">Link 2 </a> 
-</body>
+<html><head><meta name="viewport" content="width=device-width, initial-scale=1">
+</head><body style="
+    max-width: 80%;
+    margin: 20px auto;
+    text-align: center;
+">
+    <h1>Pagina di iscrizione Conferenza Gans 2021-2022</h1><br>
+    Per procedere con l'iscrizione premere uno dei due seguenti link: <br>
+    <a href="https://form.jotform.com/213096726972364" style="
+    margin: 30px;
+    display: inline-block;
+    border: 2px solid blue;
+    width: 200px;
+    text-decoration: auto;
+    color: black;
+    font-weight: bold;
+    border-radius: 9px;
+">Link 1 </a>   
+    <a href="https://form.jotform.com/213375514018350" style="margin: 30px;display: inline-block;border: 2px solid blue;width: 200px;text-decoration: auto;color: black;font-weight: bold;border-radius: 9px;">Link 2 </a> 
+
+</body></html>
