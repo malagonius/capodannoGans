d4a70b4c2a902a58e454a418ff1a7f9dc6909b67
malagonius <47065208+malagonius@users.noreply.github.com>
2021-12-06 19:54:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,194 @@
-<html><head><meta name="viewport" content="width=device-width, initial-scale=1">
+<html><head><meta name="viewport" content="width=device-width, initial-scale=1"><style>@import url("https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=devanagari,latin-ext");
+body {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #010615;
+  color: white;
+  flex-direction:column;
+  font-family: "Poppins", sans-serif;
+
+}
+
+.single-card {
+  position: relative;
+  width: 300px;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 40px;
+  transition: all 0.5s;
+  background: #010615;
+}
+.single-card:hover {
+  height: 400px;
+}
+.single-card:hover .content {
+  opacity: 1;
+}
+.single-card:hover .card-img img {
+  opacity: 1;
+}
+.single-card::before {
+  content: "";
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  right: -2px;
+  bottom: -2px;
+  background: white;
+  z-index: -1;
+  background: linear-gradient(45deg, #ff05ac 0%, #ff05ac 18%, aqua 100%);
+  /* w3c */
+}
+.single-card::after {
+  content: "";
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  right: -2px;
+  bottom: -2px;
+  background: white;
+  z-index: -2;
+  filter: blur(40px);
+  background: linear-gradient(45deg, #ff05ac 0%, #ff05ac 18%, aqua 100%);
+  /* w3c */
+}
+.single-card .card-img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+}
+.single-card .card-img img {
+  max-width: 100%;
+  opacity: 0.1;
+  transition: 0.5s;
+  height: 100%;
+}
+.single-card .content {
+  position: relative;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  opacity: 0;
+  transition: 0.5s;
+  display: flex;
+  justify-content: center;
+  align-items: center; margin:10px;background:black;background: rgb(0 0 0 / 60%);
+}
+.single-card .content .title {
+  font-size: 28px;
+  line-height: 10px; margin-top: 25px;
+}</style>
+
 </head><body style="
-    max-width: 80%;
-    margin: 20px auto;
     text-align: center;
 ">
-    <h1>Pagina di iscrizione Conferenza Gans 2021-2022</h1><br>
-    Per procedere con l'iscrizione premere uno dei due seguenti link: (Al momento disabilitato)<br>
-    <a href="https://form.jotform.com/213096726972364" style="
-    margin: 30px;
-    display: none;
+    <div class="single-card header" style="
+    margin-top: 1350px;
+    /* overflow: hidden; */
+    width: 80%;
+    flex-wrap: wrap;
+    height: 550px;
+"><h1>Pagina di iscrizione Conferenza Gans 2021-2022</h1>
+    <span style="
+    display: inline-block;
+">Per procedere con l'iscrizione premere uno dei due seguenti link: (Al momento disabilitato)</span>
+<div style="
+    display: flex;
+    flex-direction: row;
+    margin: 15px;
+"><a href="https://form.jotform.com/213096726972364" style="
+    /* margin: 10px; */
+    /* display: none; */
+    display: inline-block;
     border: 2px solid blue;
-    width: 200px;
+    width: 100%;
     text-decoration: auto;
-    color: black;
+    color: white;
     font-weight: bold;
     border-radius: 9px;
-">Link 1 </a>   
-    <a href="https://form.jotform.com/213375514018350" 
-       style="margin: 30px;
-        display: none;
-        border: 2px solid blue;
-        width: 200px;
-        text-decoration: auto;
-        color: black;
-        font-weight: bold;
-        border-radius: 9px;">Link 2 
-    </a>
-<br><br><span style="
+    margin: 5px;
+">Primo Link d'iscrizione </a><a href="https://form.jotform.com/213375514018350" style="
+                                                              /* display: none; */
+                                                              display: inline-block;
+                                                              border: 2px solid blue;
+                                                              width: 100%;
+                                                              text-decoration: auto;
+                                                              color: white;
+                                                              font-weight: bold;
+                                                              border-radius: 9px;
+                                                              margin: 5px;
+                                                              ">Secondo Link d'iscrizione</a></div><span style="
     display: none;
+    /* display: block; */
+    margin-top: 20px;
 ">
 I due link sono identici e si può usare sia uno che l'altro per iscriversi.<br>
  
 Qualora non funzionasse uno dei due, potete semplicemente utilizzare l'altro
 
 
-</span><br>
+</span></div>
+       
+    
+<div class="single-card">
+  <div class="card-img ">
+    <img src="https://i.pinimg.com/originals/f6/45/51/f6455156089967c9b421335e13a00d6f.jpg">
+  </div>
+  <div class="content">
+    <div class="title">Tema</div>
+    <p>Il tema della conferenza e’ “IL VALORE DELLE ANIME E’ GRANDE AGLI OCCHI DI DIO. “ (D&amp;A 18:10)
+
+Desideriamo aiutarti a conoscere più’ a fondo il tuo inestimabile valore.
+Ci auguriamo che tu possa divertirti ed edificarti tramite questa esperienza che potrai condividere con i tuoi cari amici.</p>
+  </div>
+</div>
+
+<div class="single-card">
+  <div class="card-img ">
+    <img src="https://images.squarespace-cdn.com/content/v1/57a88fd229687fc5a27e6cc9/1550589610645-9K6W3XT6XFR7DUVF68TN/ke17ZwdGBToddI8pDm48kIgp7GElXMh4-SI34RhNQQ4UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcPu1vjgtapbSeVJaFFpVL9g2w53smuO-pTTrV2JyYSPIFox20tTbdaQJickEwn0_f/Kindnessrevisedlow.jpg?format=2500w">
+  </div>
+  <div class="content">
+    <div class="title">Costo</div>
+    <p>Questo e’ reso possibile grazie all’aiuto dell’Area Europa che, con una piccola eccezione e visto il successo della conferenza passata, ha deciso di sostenerci nuovamente a livello economico. E’ per questo motivo infatti che da un totale di € 260 per partecipante, pagherai soltanto € 110.
+La conferenza avrà’ luogo presso Park Hotel Jolanda (San Zeno di Montagna) e potrai partecipare se hai dai 18 ai 30 anni.</p>
+  </div>
+</div>
+
+<div class="single-card">
+  <div class="card-img ">
+    <img src="https://thumbs.dreamstime.com/b/cartoon-dog-cat-show-love-vector-illustration-together-forever-cute-pets-characters-115176064.jpg">
+  </div>
+  <div class="content">
+    <div class="title">Iscrizioni</div>
+    <p>Le iscrizioni si apriranno il 9 Dicembre 2021 alle ore 20:00. I posti sono limitati (150), assicurati di aver impostato una sveglia o un promemoria sul tuo telefono per non perderti questa occasione. Ricorda, pero’, che prima di iscriverti devi aver sostenuto un’intervista con il tuo vescovo. Affrettati a contattarlo!</p>
+  </div>
+</div>
+
+
+<div class="single-card">
+  <div class="card-img">
+    <img src="https://adocnazionale.it/wp-content/uploads/2021/09/icona-green-pass-1024x1024.jpg">
+  </div>
+  <div class="content">
+    <div class="title">Green Pass</div>
+    <p>Ti ricordiamo che per partecipare sarà’ necessario essere munito di Green Pass o Super Green Pass. Qualora tu non fossi munito del “Super” potresti incorrere in limitazioni. Maggiori informazioni seguiranno in questi giorni. </p>
+  </div>
+</div><div class="single-card">
+  <div class="card-img">
+    <img src="https://www.androidworld.it/wp-content/uploads/2015/10/Whatsapp-Web.jpg">
+  </div>
+  <div class="content">
+    <div class="title">WhatsApp</div>
+    <p>Iscrivendoti verrai aggiunto al gruppo Whatsapp dove invieremo maggiori dettagli. Tieniti aggiornato seguendo anche la pagina Instagram e Facebook!</p>
+  </div>
+</div>
 </body></html>
