ee3573f5509d6aa68eaa1905148e785543b7f198
malagonius <47065208+malagonius@users.noreply.github.com>
2021-12-06 17:03:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     text-align: center;
 ">
     <h1>Pagina di iscrizione Conferenza Gans 2021-2022</h1><br>
-    Per procedere con l'iscrizione premere uno dei due seguenti link: <br>
+    Per procedere con l'iscrizione premere uno dei due seguenti link: (Al momento disabilitato)<br>
     <a href="https://form.jotform.com/213096726972364" style="
     margin: 30px;
     display: none;
@@ -16,17 +16,23 @@
     font-weight: bold;
     border-radius: 9px;
 ">Link 1 </a>   
-    <a href="https://form.jotform.com/213375514018350" style="margin: 30px;
-                                                              display: none;
-                                                              border: 2px solid blue;
-                                                              width: 200px;
-                                                              text-decoration: auto;
-                                                              color: black;
-                                                              font-weight: bold;
-                                                              border-radius: 9px;">Link 2 </a>
-<br><br>
-I due link sono identici e si può usare sia uno che l'altro per iscriversi.<br> 
+    <a href="https://form.jotform.com/213375514018350" 
+       style="margin: 30px;
+        display: none;
+        border: 2px solid blue;
+        width: 200px;
+        text-decoration: auto;
+        color: black;
+        font-weight: bold;
+        border-radius: 9px;">Link 2 
+    </a>
+<br><br><span style="
+    display: none;
+">
+I due link sono identici e si può usare sia uno che l'altro per iscriversi.<br>
+ 
 Qualora non funzionasse uno dei due, potete semplicemente utilizzare l'altro
 
 
+</span><br>
 </body></html>
